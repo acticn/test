cb7f114820110d467761c649a05d22bac176f097
acticn <34836933+acticn@users.noreply.github.com>
2021-11-05 22:01:46 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main () {
     complex c1(3,4);
     complex c2(5,6);
-
-    cout << (c1 + c2) << endl;
+    complex c3 = c1 + c2;
+    cout << c3 << endl; //(8,10)
+    cout << (c1 + c2) << endl;//错误
     return 0;
 }
